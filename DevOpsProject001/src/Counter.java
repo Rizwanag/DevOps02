5a1fc48a121ac7623c77daf73229a8ae8e6c7aab
ribegum <rizwana.begum@aig.com>
2018-08-01 09:16:58 +0530

    Rizwana local
    local rizwana commit

---
+++ DevOpsProject001/src/Counter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author RBegum
+ *
+ */
+public class Counter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter a word");
+		System.out.println("The enter word is : "+scan.nextLine());
+	}
+
+}
